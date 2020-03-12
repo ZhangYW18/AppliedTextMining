ac155c078521133ba89db19042c91f4033879c2c
Yiwei <stateofgrace@sina.com>
2020-03-12 22:21:42 +0800

    data cleaning

---
+++ Data_Cleaning.md
@@ -0,0 +1,6 @@
+# Data Cleaning
+
+1. 去除@username
+2. 去除url（://）
+3. 缩写：一个(') 忽略
+4. 单词
---
+++ code/clean.py
@@ -0,0 +1,41 @@
+""" this file is for data cleaning """
+
+import pandas as pd
+import re
+
+from nltk import SnowballStemmer
+from nltk.corpus import stopwords
+
+LOAD_FILE_NAME = '../data/training.1600000.processed.noemoticon.csv'
+STORE_FILE_NAME = '../data/tweets.csv'
+DATASET_COLUMNS = ["target", "id", "date", "flag", "user", "text"]
+DATASET_ENCODING = "ISO-8859-1"
+TEXT_CLEANING_RE = r'@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+'
+TEST = True   # if TEST=True then fetch a smaller dataset
+
+
+def read_tweets():
+    tweets = pd.read_csv(LOAD_FILE_NAME, encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+    if TEST:
+        tweets = tweets.head(int(len(tweets) / 10000))
+    print(tweets['text'])
+    print("Read csv successfully, number of tweets: " + str(len(tweets)) + '\n')
+    return tweets
+
+
+def preprocess(tweet):
+    stop_words = stopwords.words("english")
+    stemmer = SnowballStemmer("english")
+    tweet = re.sub(TEXT_CLEANING_RE, ' ', str(tweet).lower()).strip()
+    tokens = []
+    for token in tweet.split():
+        if token not in stop_words:
+            tokens.append(stemmer.stem(token))
+    return " ".join(tokens)
+
+
+if __name__ == '__main__':
+    tweets = read_tweets()
+    tweets['text'] = tweets['text'].apply(lambda x: preprocess(x))
+    print(tweets.head())
+    tweets.to_csv(STORE_FILE_NAME, encoding=DATASET_ENCODING)
